2d64f9b613c55a440efc636daf82d42cecf52e00
Hector Lima <hectoraugusto12@yahoo.ie>
2020-11-05 00:15:55 +0000

    add - sound and imgs

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById("canvas");
 const btnStart = document.getElementById("start-button");
+const btnRestart = document.getElementById("restart");
 const ctx = canvas.getContext("2d");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+canvas.width=document.documentElement.clientWidth * 0.99;
+canvas.height=document.documentElement.clientHeight * 0.98;
 
 
 const houseImg = new Image();
@@ -12,25 +13,36 @@ const peppaImg = new Image();
 peppaImg.src = "./img/peppa.png";
 
 
-let img1 = "./img/ball1.png";
-let img2 = "./img/dinossauro1.png";
-let img3 = "./img/icecream1.png";
-const obsImg = new Image();
+let img1 =  new Image();
+img1.src = "./img/ball1.png";
+let img2 = new Image();
+img2.src = "./img/dinossauro1.png";
+let img3 = new Image();
+img3.src = "./img/icecream1.png";
 const imagens = [img1,img2,img3];
-obsImg.src = imagens[Math.floor(Math.random() * imagens.length)];
 
 
 const crashAudio = new Audio();
 crashAudio.src = "./sounds/crash.mp3";
 crashAudio.volume = 0.5; 
 
+const hitAudio = new Audio();
+hitAudio.src = "./sounds/obshit.mp3"
+hitAudio.volume = 0.5;
+
+const failAudio = new Audio();
+failAudio.src = "./sounds/fail.mp3"
+failAudio.volume = 0.5;
+
+
 class Component {
-  constructor(x, y, width, height, speed) {
+  constructor(x, y, width, height, speed, img) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.speed = speed;
+    this.img = img;
   }
 
   left() {
@@ -68,7 +80,7 @@ class Obstacle extends Component {
      
     draw() {       
         
-         ctx.drawImage(obsImg, this.x, this.y, this.width, this.height);     
+         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);     
         
       
     }
@@ -82,6 +94,7 @@ class Obstacle extends Component {
       this.frames = 0;
       this.score = 0;
       this.obstacles = [];
+      this.drop = 0;
     }
   
     updateGame = () => {
@@ -96,6 +109,7 @@ class Obstacle extends Component {
       this.updateObstacles();
   
       this.updateScore(this.score);
+      this.updateDropped();
   
       this.animationId = requestAnimationFrame(this.updateGame);
   
@@ -105,18 +119,16 @@ class Obstacle extends Component {
     updateObstacles = () => {
       this.frames++;
   
-      if (this.frames % 30 === 0) {
-        this.score++;
-      }
+     
   
       this.obstacles.map((obstacle) => {
         obstacle.move();
         obstacle.draw();
       });
   
-      if (this.frames % 45 === 0) {
+      if (this.frames % 75 === 0) {
         let y = 0;
-  
+        const numberRan = Math.floor(Math.random() * imagens.length);
         let minWidth = 80;
         let maxWidth = 80;
         let width = Math.floor(
@@ -127,18 +139,37 @@ class Obstacle extends Component {
         let maxX = canvas.width - 40 - width;
         let x = Math.floor(Math.random() * (maxX - minX + 1) + minX); 
   
-        const obstacle = new Obstacle(x, y, 70, 70, 5);
+        const obstacle = new Obstacle(x, y, 70, 70, 3, imagens[numberRan]);
   
         this.obstacles.push(obstacle);
       }
     }; 
   
     checkGameOver = () => {
-      const crashed = this.obstacles.some((obstacle) => {
-        return this.player.isCrashedWith(obstacle);
+    let obsIndex;
+      const crashed = this.obstacles.some((obstacle, i) => {
+
+
+          if(obstacle.y > canvas.height ) {
+            this.obstacles.splice(i, 1);
+              this.drop++; 
+              failAudio.play();                   
+          }
+          
+          if(this.player.isCrashedWith(obstacle)){
+            obsIndex = i;
+            hitAudio.play();
+          }
+         return this.player.isCrashedWith(obstacle);
       });
   
       if (crashed) {
+            this.obstacles.splice(obsIndex, 1);
+            this.score++;
+            
+      }
+
+      if(this.drop > 4) {
         cancelAnimationFrame(this.animationId);
   
         crashAudio.play();
@@ -151,15 +182,34 @@ class Obstacle extends Component {
         ctx.fillText("Game Over!", canvas.width / 2.5, 200);
         ctx.fillStyle = "white";
         ctx.fillText(`Your Final Score: ${this.score}`, canvas.width / 3, 400);
-      }
-    };
+        btnRestart.parentElement.style.display = "block";
+         document.getElementById("restart").onclick = () => {
+           startGame();
+       }
+          startGame();
+          };
+        
+      
+      
+      console.log(this.caiu);
+      
+         
+    }; 
   
     updateScore = (score) => {
       ctx.font = "30px Verdana";
       ctx.fillStyle = "pink";
   
-      ctx.fillText(`Score ${this.score}`, 70, 20);
+      ctx.fillText(`SCORE ${this.score}`, 80, 40);
     };
+
+    updateDropped = () => {
+        ctx.font = "30px Verdana";
+        ctx.fillStyle = "pink";
+    
+        ctx.fillText(`DROPPED ${this.drop}`, 1400, 40);
+      };
+
   }
   
   class Background {
@@ -219,7 +269,8 @@ class Obstacle extends Component {
   
     function startGame() {
        
-       btnStart.parentElement.style.display = "none";
+       btnStart.parentElement.style.display = "none"; 
+       //btnRestart.parentElement.style.display = "none"      
        canvas.style.display = "block";
 
       const game = new Game(
@@ -231,11 +282,11 @@ class Obstacle extends Component {
   
       document.addEventListener("keydown", (event) => {
         if (event.key === "ArrowLeft") {
-          game.player.speed = -4;
+          game.player.speed = -6;
         }
   
         if (event.key === "ArrowRight") {
-          game.player.speed = 4;
+          game.player.speed = 6;
         }
       });
   
